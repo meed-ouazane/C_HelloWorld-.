9e47ffcdfcfeab9e58420456600cce6b86bc71ed
Meed ouazane <meedouazane2013@gmail.com>
2023-05-30 21:55:01 +0100

    My First program

---
+++ ModuleCalc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+       int num1, num2, Mod;
+       printf("Enter the first number :");
+       scanf ("%d", &num1);
+       printf("Enter the second number :");
+       scanf("%d", &num2);
+       Mod = num1 % num2;
+
+       printf("the answer is :%d\n", Mod);
+       return 0;
+
+}	
